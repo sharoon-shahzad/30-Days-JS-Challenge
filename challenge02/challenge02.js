29a30dff8f02bc3d4b14d460902c3e1f77962cfa
sharoon <kpsharoon7@gmail.com>
2024-07-14 12:45:44 +0500

    Day2 challenge completed

---
+++ challenge02/challenge02.js
@@ -0,0 +1,47 @@
+
+//Task 01 :
+let num = 20;
+let num2 =10;
+let res = num + num2;
+console.log(`The sum of ${num} and ${num2} is ${res}`);
+
+//Task 02 :
+console.log(`The subtraction of ${num} and ${num2} is ${num - num2}`);
+
+//Task 03 :
+console.log(`The multiplication of ${num} and ${num2} is ${num * num2}`);
+
+//Task 04:
+console.log(`The division of ${num} and ${num2} is ${num / num2}`);
+
+// Task 05:
+console.log(`The reminder of ${num} and ${num2} is ${num % num2}`);
+
+// Task 06:
+console.log(`The result of adding number to variable is ${num+=5}`);
+
+// Task 07:
+console.log(`The result of subtrating number from variable is ${num-=5}`);
+
+// Task 08:
+console.log(num > num2);
+console.log(num < num2);
+
+//Task 09:
+console.log(num >= num2);
+console.log(num <= num2);
+
+//Task 10:
+console.log(num == num2);
+console.log(num === num2);
+
+//Task 11:
+console.log(num > num2 && num < num2);
+
+//Task 12:
+console.log(num > num2 || num < num2);
+
+//Task 13:
+console.log(!(num < num2));
+//Task 14:
+console.log(num < num2 ? true : false);
